1fae6bacb8cd07d2b51eccf8d0dfdfdb8b9f04e0
Nspired1 <don.spire1@gmail.com>
2020-09-27 16:45:19 -0700

    form validation for name field

--- src/components/AddContact.js
+++ src/components/AddContact.js
@@ -8,11 +8,17 @@ class AddContact extends Component {
     name: "",
     email: "",
     phone: "",
+    errors: { name: "" },
   };
 
   onSubmit = (dispatch, e) => {
     e.preventDefault();
     const { name, email, phone } = this.state;
+    //validation for name field, user is forced to enter a name for contact
+    if (name === "") {
+      this.setState({ errors: { name: "Name is REQUIRED" } });
+      return;
+    }
     const newContact = {
       id: uuidv4(),
       name,
@@ -22,10 +28,12 @@ class AddContact extends Component {
     dispatch({ type: "ADD_CONTACT", payload: newContact });
 
     //clears component state to reset form
+    //also clears errors object so errors aren't propagated
     this.setState({
       name: "",
       email: "",
       phone: "",
+      errors: {},
     });
   };
 
@@ -33,6 +41,9 @@ class AddContact extends Component {
 
   //Below shows 2 ways of getting inputs from forms, one uses a functional component <TextInputGroup /> and the other uses JSX markup class="form-group"
   render() {
+    //destructured errors from State to avoid TypeError since errors doesn't have a name property unless name is blank
+    //and errors.name won't propagate down to TextInputGroup to show an error message for blank name
+    const { errors } = this.state;
     return (
       <Consumer>
         {(value) => {
@@ -49,6 +60,7 @@ class AddContact extends Component {
                     placeholder="Enter Name"
                     value={this.state.name}
                     onChange={this.onChange}
+                    error={errors.name}
                   />
                   <TextInputGroup
                     label="Email"
--- src/components/TextInputGroup.js
+++ src/components/TextInputGroup.js
@@ -1,4 +1,5 @@
 import React from "react";
+import classnames from "classnames";
 import PropTypes from "prop-types";
 
 //destructure the props object for brevity in component
@@ -9,6 +10,7 @@ const TextInputGroup = ({
   placeholder,
   type,
   onChange,
+  error,
 }) => {
   return (
     <div className="form-group">
@@ -17,10 +19,13 @@ const TextInputGroup = ({
         type={type}
         name={name}
         value={value}
-        className="form-control form-control-lg"
+        className={classnames("form-control form-control-lg", {
+          "is-invalid": error,
+        })}
         placeholder={placeholder}
         onChange={onChange}
       />
+      {error && <div className="invalid-feedback">{error}</div>}
     </div>
   );
 };
@@ -32,6 +37,7 @@ TextInputGroup.propTypes = {
   value: PropTypes.string.isRequired,
   placeholder: PropTypes.string.isRequired,
   onChange: PropTypes.func.isRequired,
+  error: PropTypes.string.isRequired,
 };
 
 export default TextInputGroup;
