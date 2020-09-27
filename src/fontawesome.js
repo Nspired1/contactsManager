cb487a290b71127741f0be293fbf04125d8e1bf5
Nspired1 <don.spire1@gmail.com>
2020-09-26 18:55:40 -0700

    add fontawesome file

---
+++ src/fontawesome.js
@@ -0,0 +1,10 @@
+//import font awesome library
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+//import icons
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { faSortDown } from "@fortawesome/free-solid-svg-icons";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
+
+//add icons to library
+library.add(faPlus, faSortDown, faTimes);
