978c5415b767494a530ae76dcd7a9f6bf4f5c7c2
Nspired1 <don.spire1@gmail.com>
2020-09-27 15:47:13 -0700

    updated files for new features and packages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "./components/Header";
 import Contacts from "./components/Contacts";
+import AddContact from "./components/AddContact";
 import { Provider } from "./context";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -11,6 +12,7 @@ function App() {
       <div className="App">
         <Header branding="Contact Manager 1.0" />
         <div className="container">
+          <AddContact />
           <Contacts />
         </div>
       </div>
