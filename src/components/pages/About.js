558767d307474b97421f3c23b129afee3bd3d999
Nspired1 <don.spire1@gmail.com>
2020-09-29 23:22:36 -0700

    added About and NotFound pages

---
+++ src/components/pages/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div>
+      <h1 className="display-4">About Contact Manager</h1>
+      <p className="lead">
+        Basic app to organize contacts and practice React, Context, React
+        Router, Bootstrap 4, etc, etc.
+      </p>
+      <p className="text-secondary">Copyright 2020, Version 1.0</p>
+    </div>
+  );
+}
