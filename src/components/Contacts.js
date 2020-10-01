dc0b038c232915d5cd4a06f19984de1a27bdea9d
Nspired1 <don.spire1@gmail.com>
2020-10-01 00:27:22 -0700

    update for Redux

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,29 +1,40 @@
 import React, { Component } from "react";
 import Contact from "./Contact";
-import { Consumer } from "../context";
+import { connect } from "react-redux";
+import { GET_CONTACTS } from "../../actions/types";
+import PropTypes from "prop-types";
 
 class Contacts extends Component {
-  render() {
-    return (
-      <Consumer>
-        {(value) => {
-          //destructuring contacts object for brevity in Contact component
-          const { contacts } = value;
+  componentDidMount() {
+    this.props.getContacts();
+  }
 
-          return (
-            <div>
-              <h1 className="display-4 mb-4">
-                <span className="text-danger">Contacts</span> Manager
-              </h1>
-              {contacts.map((contact) => (
-                <Contact key={contact._id} contact={contact} />
-              ))}
-            </div>
-          );
-        }}
-      </Consumer>
+  render() {
+    const { contacts } = this.props;
+    return (
+      <div>
+        <h1 className="display-4 mb-4">
+          <span className="text-danger">Contacts</span> Manager
+        </h1>
+        {contacts.map((contact) => (
+          <Contact key={contact._id} contact={contact} />
+        ))}
+      </div>
     );
   }
 }
 
-export default Contacts;
+Contacts.propTypes = {
+  contacts: PropTypes.array.isRequired,
+  getContacts: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  contacts: state.contact.contacts,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  getContacts: () => dispatch({ type: GET_CONTACTS }),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Contacts);
--- src/reducers/contactReducer.js
+++ src/reducers/contactReducer.js
@@ -1,7 +1,22 @@
-const initialState = {};
+import { GET_CONTACTS } from "../actions/types";
+
+const initialState = {
+  contacts: [
+    {
+      _id: 1,
+      name: "Jane Doe",
+      email: "jdoe@gmail.com",
+      phone: "555-555-5555",
+    },
+  ],
+};
 
 export default function (state = initialState, action) {
   switch (action.type) {
+    case GET_CONTACTS:
+      return {
+        ...state,
+      };
     default:
       return state;
   }
